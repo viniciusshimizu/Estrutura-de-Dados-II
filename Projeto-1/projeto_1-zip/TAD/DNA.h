a207233c29fcc2142d16c692a8a2ee30331b8e28
Vinicius Shimizu <43760678+viniciusshimizu@users.noreply.github.com>
2022-06-15 20:32:56 +0000

    criação .h

---
+++ Projeto-1/projeto_1-zip/TAD/DNA.h
@@ -0,0 +1,11 @@
+
+
+
+void OrdenaDigitos(A, n, posicao);
+void OrdenaNumeros(A,n);
+??? ContagemIntersecoes( arquivo A, arquivo B, nA, nB, arquivo saida)
+
+??? CtrlF(arquivo texto, arquivo trechos, arquivo saida)
+
+
+
